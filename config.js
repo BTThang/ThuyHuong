64d152b294d6c0485f4e0ad084de68833eb61727
ThangBuiTan <72704514+BTThang@users.noreply.github.com>
2024-12-18 01:26:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameFriend = 'Long\'s Graduation Day';
-const giftUrl = 'card.png';
+const giftUrl = 'invitation.png';
 const eventName = 'You are invited';
 const titleCard = 'Long\'s Graduation Day';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
@@ -8,6 +8,6 @@ const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và nh�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'card.png';
+const giftImage = 'invitation.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
